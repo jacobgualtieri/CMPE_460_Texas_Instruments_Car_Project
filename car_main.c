d3cc10fd5ce547f7b7c6c8731d8d4ef708a2cc38
Jacob G <jgualtie@icloud.com>
2022-03-21 13:46:30 -0400

    Added includes and camera support code to car_main

--- car_main.c
+++ car_main.c
@@ -1,3 +1,13 @@
+/*
+* Rochester Institute of Technology
+* Department of Computer Engineering
+* CMPE 460  Interfacing Digital Electronics
+* Jacob Gualtieri & Zebulon Hollinger
+* 3/21/2022
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "Common.h"
 #include "oled.h"
 #include "msp.h"
@@ -12,7 +22,55 @@
 uint16_t line[128];
 BOOLEAN g_sendData;
 
+/**
+ * @brief simple delay function
+ * 
+ */
+void myDelay(void){
+	volatile int j;
+	for (j = 0; j < 800000; j++){}
+}
+
+/**
+ * @brief camera initialization function
+ * 
+ */
+void INIT_Camera(void){
+	g_sendData = FALSE;
+	ControlPin_SI_Init();
+	ControlPin_CLK_Init();
+	ADC0_InitSWTriggerCh6();
+}
+
+
 int main(void){
-    for(;;){
+
+    unsigned int i = 0;
+
+    /* Initializations */
+    DisableInterrupts();
+
+    LED1_Init();
+    LED2_Init();
+    INIT_Camera();
+
+    /* Begin Infinite Loop */
+    EnableInterrupts();
+
+    for (;;){
+        if (g_sendData == TRUE){
+			LED1_On();
+			for (i = 0; i < 128; i++){
+
+                // TODO: Parse camera data (line)
+				//sprintf(str,"%i\n\r", line[i]);
+                
+				uart0_put(str);
+			}
+			g_sendData = FALSE;
+			LED1_Off();
+		}
+		// do a small delay
+		myDelay();
     }
 }
