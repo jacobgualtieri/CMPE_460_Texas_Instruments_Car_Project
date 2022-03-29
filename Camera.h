b44c6dc2830b7fc8448ea8a795ab61db3e603146
Jacob G <jgualtie@icloud.com>
2022-03-28 23:16:28 -0400

    she really do be wildin tho

--- Camera.h
+++ Camera.h
@@ -4,9 +4,14 @@
 #define _CAMERA_H_
 #endif
 
+typedef struct max_and_mins {
+    uint16_t max;
+    uint16_t min;
+} max_and_mins_t;
+
 void INIT_Camera(void);
 void readCameraData(uint16_t* raw_camera_data);
-uint16_t MovingAverage(uint16_t* line_data, uint16_t* smoothed_line);
+max_and_mins_t MovingAverage(uint16_t* line_data, uint16_t* smoothed_line);
 void split_average(uint16_t* line_data, uint16_t* avg_line_data);
 void slope_finder(uint16_t* line_data, int* result_array);
 int determine_direction(uint16_t* avg_line_data);
