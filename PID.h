7b0f2356ab4e76fb644d2dfc0b2a35e45baf0f7e
[zebulon0] <zah1276@rit.edu>
2022-04-13 21:57:39 -0400

    Cleaned up PID.h

--- PID.h
+++ PID.h
@@ -1,31 +1,22 @@
 #include <stdlib.h>
 #include <stdint.h>
-
 #ifndef _STEERING_PID_
 #define _STEERING_PID_
 #endif
 
 #define HISTORY_LENGTH 3
-
-#define STRAIGHT   0.075
 #define FULL_LEFT  0.11
 #define FULL_RIGHT 0.05
 
 /* Midpoint Calculation */
 #define MIDPOINT(L_IDX,R_IDX) (((L_IDX) + (R_IDX))/2)
 
-#define UPDATE_PID(PID_STRUCT,NEW_KP,NEW_KI,NEW_KD){ \
-    (PID_STRUCT).kp = (NEW_KP); \
-    (PID_STRUCT).ki = (NEW_KI); \
-    (PID_STRUCT).kd = (NEW_KD); \
-}
 
 typedef struct pid_values {
     float kp;
     float ki;
     float kd;
 } pid_values_t;
-
 
 void PrintSteeringValues(pid_values_t steering_values);
 void PrintDrivingValues(pid_values_t driving_values);
