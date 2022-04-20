c729f06bdd81adf3af9f0f6b122ae083e52adc8e
[zebulon0] <zah1276@rit.edu>
2022-04-20 10:12:45 -0400

    Updated low pass filter removed unused function

--- Camera.c
+++ Camera.c
@@ -1,10 +1,7 @@
 #include <stdint.h>
-#include <stdio.h>
 #include "Common.h"
 #include "ControlPins.h"
 #include "ADC14.h"
-#include "leds.h"
-#include "uart.h"
 #include "Camera.h"
 
 extern BOOLEAN g_sendData;
@@ -17,31 +14,6 @@ void INIT_Camera(void){
     ControlPin_SI_Init();
     ControlPin_CLK_Init();
     ADC0_InitSWTriggerCh6();
-}
-
-void readCameraData(uint16_t* raw_camera_data){
-    // TODO: Re-evaluate the purpose of this function
-    //  I don't think we even use this anymore
-    char str[100];
-    int i;
-    if (g_sendData == TRUE){
-        LED1_On();
-
-        // send the array over uart
-        sprintf(str,"%i\n\r",-1); // start value
-        uart0_put(str);
-        uart2_put(str);
-        for (i = 0; i < 128; i++){
-            sprintf(str,"%i\n\r", raw_camera_data[i]);
-            uart0_put(str);
-            uart2_put(str);
-        }
-        sprintf(str,"%i\n\r",-2); // end value
-        uart0_put(str);
-        uart2_put(str);
-        LED1_Off();
-        g_sendData = FALSE;
-    }
 }
 
 /**
@@ -69,25 +41,27 @@ void FindCenterOfMass(uint16_t* smoothed_line, center_of_mass_t* stat_collection
 }
 
 /**
- * @brief Calculates the 5-point moving average of the camera line data
- * and determines the maximum value detected by the camera
+ * @brief Calculates the 7-point moving average of the camera line data.
+ * This is a digital low pass filter with kernel size 7 and a gain of 1
  * 
  * @param line_data raw camera data
  * @param smoothed_line smoothed camera data after averaging
- * @param stat_collection contains values and indexes of the max and min
- * values of the raw camera data
  */
 void MovingAverage(uint16_t* line_data, uint16_t* smoothed_line){
-    int i;
-    uint16_t five_p_avg;    // 5 point average value
+    uint16_t avg;    // 7 point average value
 
-    for(i = 2; i < 126; i++){
-        five_p_avg = line_data[i+2]/5 + line_data[i+1]/5 + line_data[i]/5 + line_data[i-1]/5 + line_data[i-2]/5;
-        smoothed_line[i] = five_p_avg;
+    for(int i = 3; i < 125; i++){
+        avg = line_data[i+3]/7 + line_data[i+2]/7 + line_data[i+1]/7 + line_data[i]/7 + line_data[i-1]/7 + line_data[i-2]/7 + line_data[i-3]/7;
+        smoothed_line[i] = avg;
     }
 
-    smoothed_line[0] = line_data[2];
-    smoothed_line[1] = line_data[2];
-    smoothed_line[126] = line_data[125];
-    smoothed_line[127] = line_data[125];
+    //  First 3 values in smoothed array get set to the first element to get averaged
+    smoothed_line[0] = smoothed_line[3];
+    smoothed_line[1] = smoothed_line[3];
+    smoothed_line[2] = smoothed_line[3];
+
+    //  Last 3 values in the array get set to the last element to get averaged
+    smoothed_line[125] = smoothed_line[124];
+    smoothed_line[126] = smoothed_line[124];
+    smoothed_line[127] = smoothed_line[124];
 }
--- Camera.h
+++ Camera.h
@@ -9,6 +9,5 @@ typedef struct center_of_mass {
 } center_of_mass_t;
 
 void INIT_Camera(void);
-void readCameraData(uint16_t* raw_camera_data);
 void MovingAverage(uint16_t* line_data, uint16_t* smoothed_line);
 void FindCenterOfMass(uint16_t* smoothed_line, center_of_mass_t* stat_collection);
